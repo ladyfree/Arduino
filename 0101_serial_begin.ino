e7a345e6cdde6f51f370eb43098701da97ef8acf
ladyfree <83199278+ladyfree@users.noreply.github.com>
2022-12-06 02:49:54 +0900

    Add files via upload

---
+++ 0101_serial_begin.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  // 한번만 실행되는 곳
+  Serial.begin(9600);
+  Serial.print("this is setup()\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("this is loop()\n");
+
+}
